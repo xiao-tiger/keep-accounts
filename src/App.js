7103ccc23a5fb82201f7b9ffd0236947c0a81f93
HAHA <1497154694@qq.com>
2021-01-16 13:21:13 +0800

    引入路由

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import { withRouter, Route } from 'react-router-dom'
+import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 import Home from './containers/Home'
+import Create from './containers/Create'
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/create" component={Create} />
+      <Route path="/edit/:id" component={Create} />
     </div>
   )
 }
 
-export default App;
+export default withRouter(App);
 
 
 
---
+++ src/containers/Create.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+class Create extends React.PureComponent {
+
+  render() {
+    return (
+      <div>
+        <h4>Create</h4>
+      </div>
+    )
+  }
+}
+
+export default Create
